cfed9cfd6e3bff4eded50bf3739c9aea8dff6d9f
junaid53 <ansarijunaid53@gmial.com>
2024-07-15 17:55:39 +0530

    backedn icon title

--- resources/views/backend/partials/meta.blade.php
+++ resources/views/backend/partials/meta.blade.php
@@ -1,7 +1,8 @@
 <base id="baseUrl" href="{{url('')}}">
 <meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">    
-<title>poonam</title>
+<title>Poonam</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
 <meta content="Coderthemes" name="author">
+<link rel="icon" type="image/png" href="/assets/image/logo_1.png" sizes="16x16">
