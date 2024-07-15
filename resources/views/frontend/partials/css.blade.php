19856cf7337d200e25aa8c9be6ebc29357e0aa5a
Faisal <khanfasial.makent@gmail.com>
2024-07-15 13:00:57 +0530

    correct my code

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -2,7 +2,7 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Cinzel:400,700%7CRaleway:500,500i,600,700%7CMaterial+Icons&amp;display=swap" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Template Vendor's Stylesheet -->
-  <link rel="stylesheet" type="text/css" href="/assets/frontend/css/vendor.css" />
+  <link rel="stylesheet" type="text/css" href="/assets/frontend/css/vendor.css?<?php echo time(); ?>" />
   <!-- Template Fancy Stylesheet -->
   <link rel="stylesheet" type="text/css" href="/assets/frontend/css/jquery.fancybox.min.css" />
   <!-- Template Main Stylesheet -->
