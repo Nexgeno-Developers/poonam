ac3d62d85a918e331060edeec66581f317306952
SkZubeirAli <zuber.nexgeno@gmail.com>
2024-09-24 12:46:32 +0530

    css

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -1,6 +1,6 @@
   <!-- Google Fonts -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Cinzel:400,700%7CRaleway:500,500i,600,700%7CMaterial+Icons&amp;display=swap" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="/assets/frontend/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Template Vendor's Stylesheet -->
   <link rel="stylesheet" type="text/css" href="/assets/frontend/css/vendor.css" />
   <!-- Template Fancy Stylesheet -->
@@ -10,4 +10,4 @@
   <!-- <link rel="stylesheet" type="text/css" href="/assets/frontend/css/main.css" /> -->
   <link rel="stylesheet" type="text/css" href="/assets/frontend/css/style.css" />
   <!-- <link rel="stylesheet" type="text/css" href="/assets/frontend/css/responsive.css" /> -->
-  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+  <link rel="stylesheet" href="/assets/frontend/css/swiper-bundle.min.css">
